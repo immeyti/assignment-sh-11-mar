7616c38d5e068701c53c7577fce9ffca043cf562
immeyti <mehdi.samirakh68@gmail.com>
2023-03-11 17:37:46 +0330

    Added comments to interface

--- app/Services/Distance/DistanceUnit.php
+++ app/Services/Distance/DistanceUnit.php
@@ -4,6 +4,17 @@ namespace App\Services\Distance;
 
 interface DistanceUnit
 {
+    /**
+     * Converts the distance value to meters.
+     *
+     * @return float The distance value in meters.
+     */
     public function toMeters();
+
+    /**
+     * Converts a distance value in meters to the current distance unit.
+     *
+     * @return float The distance value in the current distance unit.
+     */
     public function fromMeters();
 }
